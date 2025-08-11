b4af3a737b33d723d896c2eed5739746c986e368
Your Name <otumsgosepel@gmail.com>
2025-08-11 11:32:21 +0100

    setting up and configuring the frontend react app

---
+++ frontend/src/home.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+function Home(){
+  return (
+    <>
+      <Button color="secondary">Click Me</Button>
+      <input type="button" value="Click me"/>
+   </>
+);
+
+}
+
+export default Home;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,21 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
